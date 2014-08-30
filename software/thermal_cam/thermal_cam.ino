6b2058fa050d1ce93c39cfeb069b4bd5f3835460
Sebastien Chassot (sinux) <sebastien.chassot@etu.hesge.ch>
2014-08-30 17:46:37 +0200

    change pics

--- software/thermal_cam/thermal_cam.ino
+++ software/thermal_cam/thermal_cam.ino
@@ -26,14 +26,14 @@
 
 #define DIRECTION_X     true
 #define DIRECTION_Y     true
-#define STP_X           2
-#define DIR_X           3
-#define MS1_X           4
-#define MS2_X           5
-#define STP_Y           6
-#define DIR_Y           7
-#define MS1_Y           8
-#define MS2_Y           9
+#define STP_X           6
+#define DIR_X           7
+#define MS1_X           8
+#define MS2_X           9
+#define STP_Y           2
+#define DIR_Y           3
+#define MS1_Y           4
+#define MS2_Y           5
 
 #define END_X           10
 #define END_Y           11
@@ -46,31 +46,28 @@ int a = 0;     //  gen counter
 
 void setup() 
 {            
-  Serial.begin(115200);
-  Serial.println("Init MLX90614 temperature Sensor");
 
-  PORTC = (1 << PORTC4) | (1 << PORTC5);  //enable internal pullup resistors on i2c ports
+    Serial.begin(115200);
+    Serial.println("Init MLX90614 temperature Sensor");
+  
+    PORTC = (1 << PORTC4) | (1 << PORTC5);  //enable internal pullup resistors on i2c ports
+  
+    Serial.println("Init motors");
+    /* init motors 
+     * *****************/
+    DDRD = B11111100;         // set pins 2-7 as OUTPUT
+    DDRB = B00001111;         // set pins 8-11 as OUTPUT
+  
+    /* Set micro stepping 
+    * pins 4,5,8,9 HIGH (1/8 step) */
+//    PORTD = B00000011;        // pins 4,5 HIGH (1/8 X step)
+//    PORTB = B00110000;        // pins 8,9 HIGH (1/8 Y step)
+  
+    if( DIRECTION_X )
+        PORTD |= (1<<DIR_X);  // toggle high/low
+    if( DIRECTION_Y )
+        PORTD |= (1<<DIR_Y);  // toggle high/low
 
-  Serial.println("Init motors");
-  /* init motors 
-   * *****************/
-  DDRD = B11111100;         // set pins 2-7 as OUTPUT
-  DDRB = B00001111;         // set pins 8-11 as OUTPUT
-
- /* Set micro stepping 
-  * pins 4,5,8,9 HIGH (1/8 step) */
-  PORTD = B00000011;        // pins 4,5 HIGH (1/8 X step)
-  PORTB = B00110000;        // pins 8,9 HIGH (1/8 Y step)
-
-  if( DIRECTION_X )
-      PORTD |= (1<<DIR_X);  // toggle high/low
-  if( DIRECTION_Y )
-      PORTD |= (1<<DIR_Y);  // toggle high/low
-
-  while(digitalRead(END_X) != HIGH ){
-      step(1, MOTX);
-      delay(4);
-  }
 
 }
 
@@ -84,19 +81,21 @@ void loop(){
     
         a++;
         errno = step(1, MOTX);
-        delay(6);              
+        delay(60);              
         readPoint();
 
     }else{
        a++;
         errno = step(-1, MOTX);
-        delay(6);
+        delay(60);
 //        readPoint();
     
         if (a>500){    //sweep 200 in dir 2
 
             delay(1000);
             a = 0;
+            if(digitalRead(10) == HIGH)
+                Serial.println("Hello switch");
 
         }
     }
@@ -177,7 +176,17 @@ long int readMLXtemperature(int TaTo) {
     return(lii*2-27315);
 }
 
-void blink()
-{
-  Serial.println("Hello world!");
+void initPos(){
+
+    int eX, eY;
+
+    eX = digitalRead(END_X);
+    eY = digitalRead(END_Y);
+
+    while(digitalRead(END_X) != HIGH ){
+        step(1, MOTX);
+        delay(4);
+    }
+
 }
+
